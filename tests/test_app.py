6f91d7de88fa41ae3e2da9d934793c0db3387e1d
Douglas Farias <farias.douglascastro@gmail.com>
2025-08-20 12:18:09 -0300

    Add initial test setup

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app.app import app
+
+@pytest.fixture(name="client")
+def app_client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b"Hello World!"
