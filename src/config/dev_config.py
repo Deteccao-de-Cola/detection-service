52e3c6ad579582dd1377448fc85b1eede0bb33d5
Douglas Farias <farias.douglascastro@gmail.com>
2025-09-02 09:27:01 -0300

    add config classes for dev and production

---
+++ src/config/dev_config.py
@@ -0,0 +1,7 @@
+# pylint: disable=invalid-name
+class DevConfig:
+    def __init__(self):
+        self.ENV = "development"
+        self.DEBUG = True
+        self.PORT = 3000
+        self.HOST = '0.0.0.0'
---
+++ src/config/production.py
@@ -0,0 +1,7 @@
+# pylint: disable=invalid-name
+class ProductionConfig:
+    def __init__(self):
+        self.ENV = "production"
+        self.DEBUG = False
+        self.PORT = 80
+        self.HOST = '0.0.0.0'
